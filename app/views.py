8daa3cbdab7b3278e2c912e349824de22fd65762
CerasCTK <ceras410@gmail.com>
2025-12-07 10:27:18 +0700

    feat: add app URL routing and integrate app.urls into project URLs

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+
+def home(_request: HttpRequest) -> HttpResponse:
+    """
+    Handle requests to the home page
+    """
+    return HttpResponse("Hello world!")
