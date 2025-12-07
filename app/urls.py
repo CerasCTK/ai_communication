8e944cbe70882230bd792c498d8a7a9116b8187b
CerasCTK <ceras410@gmail.com>
2025-12-07 11:24:21 +0700

    chore(docs): add docstrings to project modules, views, URLs, tests and app config

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,10 @@
+"""
+URL configuration for this application.
+
+This module defines the URL patterns that map incoming HTTP requests
+to the appropriate view functions within the app
+"""
+
 from django.urls import path
 from . import views
 
