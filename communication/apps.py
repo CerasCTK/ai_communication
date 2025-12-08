3ea6b7d50c99bc6b26b46b3e4ecefecdf92b7cbb
minhduy250802 <minhduy250802@gmail.com>
2025-12-08 21:17:19 +0700

    chore(websocket): register communication Django app

---
+++ communication/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class CommunicationConfig(AppConfig):
+    """
+    Django app configuration for the communication module.
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "communication"
