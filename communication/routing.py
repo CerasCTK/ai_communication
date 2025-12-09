eec66cf1c3f70b27674d31e44854d7713d33f111
minhduy250802 <minhduy250802@gmail.com>
2025-12-09 20:24:33 +0700

    refactor(websocket): remove deprecated communication module after migration

--- communication/routing.py
+++
@@ -1,7 +0,0 @@
-# communication/routing.py
-from django.urls import re_path
-from .consumers import AudioConsumer
-
-websocket_urlpatterns = [
-    re_path(r'^ws/audio/$', AudioConsumer.as_asgi()),
-]
