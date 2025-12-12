e139f643e1ccbdc6b5732833fd123025f4292df7
Ceras <62826546+CerasCTK@users.noreply.github.com>
2025-12-12 22:46:26 +0700

    Revert "chore(websocket): integrate communication routing into ASGI"
    This reverts commit 7487b40390232e911b53c2d521ab348015261e5e.

--- ai_communication/asgi.py
+++ ai_communication/asgi.py
@@ -1,22 +1,16 @@
-# ai_communication/asgi.py
+"""
+ASGI config for ai_communication project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
 import os
 
 from django.core.asgi import get_asgi_application
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-
-import communication.routing
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ai_communication.settings")
 
-django_asgi_app = get_asgi_application()
-
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(
-            URLRouter(communication.routing.websocket_urlpatterns)
-        ),
-    }
-)
+application = get_asgi_application()
