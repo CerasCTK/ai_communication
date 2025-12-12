a72ed9ef473a460035134266eed2fc2ffdb063e2
Ceras <62826546+CerasCTK@users.noreply.github.com>
2025-12-12 22:46:26 +0700

    Revert "chore(docs): add docstrings to project modules, views, URLs, tests and app config"
    This reverts commit 67851c064032f038bad179d67d70b74380b6a6e6.

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,3 @@
-"""
-URL configuration for this application.
-
-This module defines the URL patterns that map incoming HTTP requests
-to the appropriate view functions within the app
-"""
-
 from django.urls import path
 from . import views
 
--- app/views.py
+++ app/views.py
@@ -1,10 +1,3 @@
-"""
-View functions for the main application.
-
-This module defines the HTTP endpoints used to render responses
-for the root page of the application
-"""
-
 from django.http import HttpResponse, HttpRequest
 
 
