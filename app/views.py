28dee426afab726f812ec302c599690c4e0592dd
Ceras <62826546+CerasCTK@users.noreply.github.com>
2025-12-12 22:46:26 +0700

    Revert "chore(ui): add sample home template and update view for demonstrate"
    This reverts commit f8b004df8f2e164aade52971abef671b0b022c47.

--- app/views.py
+++ app/views.py
@@ -6,11 +6,10 @@ for the root page of the application
 """
 
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
 
 
-def home(request: HttpRequest) -> HttpResponse:
+def home(_request: HttpRequest) -> HttpResponse:
     """
     Handle requests to the home page
     """
-    return render(request, "app/home.html")
+    return HttpResponse("Hello world!")
