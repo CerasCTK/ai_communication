0a6fcbff92e3a0ab5545ec5157e46c184fdb4744
CerasCTK <ceras410@gmail.com>
2025-12-13 12:00:30 +0700

    refactor: remove some print statement

--- app/services/tests/test_openai.py
+++ app/services/tests/test_openai.py
@@ -6,7 +6,7 @@ from app.services.open_ai import AIUtilityClient
 class TestOpenAI(SimpleTestCase):
 
     @classmethod
-    def setUpClass(cls):
+    def setUpClass(cls) -> None:
         super().setUpClass()
         cls.API_KEY = "sk-YfSO3RgAtWp8-SBGwXva1w"
         cls.BASE_URL = "https://aiportalapi.stu-platform.live/jpe"
@@ -16,35 +16,32 @@ class TestOpenAI(SimpleTestCase):
             api_key=cls.API_KEY, base_url=cls.BASE_URL, deployment_name=cls.MODEL_NAME
         )
 
-    # -------------------------------------------------
-    def test_custom_topic_technology(self):
+    def test_custom_topic_technology(self) -> None:
+        """Test technology topic"""
         text = "AI is growing very fast and changing our future rapidly."
 
         result = self.ai.generate_feedback(text, "custom_topic")
-        print(result)
 
         self.assertIsNotNone(result)
         self.assertTrue(len(result) > 20)
 
-    # -------------------------------------------------
-    def test_custom_topic_finance(self):
+    def test_custom_topic_finance(self) -> None:
+        """Test finance topic"""
         text = "I want to invest money using compound interest and buy some stocks."
 
         result = self.ai.generate_feedback(text, "custom_topic")
-        print(result)
 
         self.assertIsNotNone(result)
         self.assertTrue(len(result) > 20)
 
-    # -------------------------------------------------
-    def test_custom_topic_travel(self):
+    def test_custom_topic_travel(self) -> None:
+        """Test travel topic"""
         text = (
             "When I travel to Europe, I often get confused at customs and immigration. "
             "I want to improve my English so I can ask for directions and talk to hotel staff more confidently."  # noqa: E501
         )
 
         result = self.ai.generate_feedback(text, "custom_topic")
-        print(result)
 
         self.assertIsNotNone(result)
         self.assertTrue(len(result) > 20)
